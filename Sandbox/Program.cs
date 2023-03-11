0ca30814266bc007f115a30bcd0c92e6e0625965
Goreeeo <endmyaddress@gmail.com>
2023-03-11 23:54:01 +0100

    Readded sandbox

---
+++ Sandbox/Program.cs
@@ -0,0 +1,38 @@
+﻿using FaddleEngine;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main()
+        {
+            _ = new Game();
+        }
+    }
+
+    public class Game : Application
+    {
+        public Game() : base(new WindowSettings { Title = "Sandbox", Size = new Vector2Int(500, 500), Fullscreen = false, BackgroundColor = Color.White, CursorVisible = true })
+        {
+
+        }
+
+        protected override void OnStart()
+        {
+            _ = new Camera(Vector3.UnitZ * 3, WindowSize.x / (float)WindowSize.y)
+            {
+                Yaw = -90f
+            };
+
+            GameObject gO = new(Vector3.Zero, Vector3.Zero, new Vector3(4f, 0.5f, 0));
+            Texture texture = new(new Color[] { Color.Black }, 1);
+            Button button = new("Hi Guys!", Font.CASCADIA, texture);
+            button.OnLeftMouseButtonDown.AddListener(() => Log.Info("Hello"));
+            gO.AddComponent(button);
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+    }
+}
